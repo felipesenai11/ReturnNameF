2c46de03fa885676800afef36d8ae413ed8e92c8
gabri <gabri@Alef>
2025-04-22 16:22:50 -0300

    ProjectName

---
+++ OneDrive/Documentos/NetBeansProjects/ReturnName/src/main/java/com/mycompany/returnname/ReturnName.java
@@ -0,0 +1,13 @@
+package com.mycompany.returnname;
+import java.util.Scanner;
+public class ReturnName {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = sc.nextLine();
+        
+            System.out.println("Seu nome: ");
+        
+    }
+}
